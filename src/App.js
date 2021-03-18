8b13ac23af3e347a767340c3c64d1e140d35d4c3
Nick Sardonini <nsardonini@gmail.com>
2021-03-18 11:30:32 -0400

    remove unnecessary imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, useRef } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import colors from "./colors";
 import styled from "styled-components";
 import Nav from "./components/nav/nav";
