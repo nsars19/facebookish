8ccd70d0c49a1403823f204d4b1dc07cb86cc11e
Nick Sardonini <nsardonini@gmail.com>
2021-03-18 21:52:25 -0400

    add homepage component
    fetches posts from the current user & current user's friends to populate the feed.

---
+++ src/components/homepage/homepage.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+import styled from "styled-components";
+import StatusForm from "./statusForm/statusForm";
+import colors from "./../../colors";
+import { Link } from "react-router-dom";
+
+const StyledHomePage = styled.div`
+  color: black;
+`;
+
+function HomePage(props) {
+  const { currentUser } = props;
+  const [feed, setFeed] = useState(null);
+
+  useEffect(() => {
+    (async function fetchFeed() {
+      const response = await fetch(
+        `http://localhost:3000/posts/feed/${currentUser}`
+      );
+      const data = await response.json();
+      setFeed(data);
+    })();
+  }, [currentUser]);
+
+  return (
+    <StyledHomePage>
+      <StatusForm colors={colors} currentUser={currentUser}></StatusForm>
+      {(feed && (
+        <div>
+          {feed.map((post) => (
+            <div key={post._id}>
+              <Link to={`/user/${post.author._id}`}>
+                {post.author.firstName} {post.author.lastName}
+              </Link>
+              <p>{post.text}</p>
+            </div>
+          ))}
+        </div>
+      )) || <h2>Loading...</h2>}
+    </StyledHomePage>
+  );
+}
+
+export default HomePage;
