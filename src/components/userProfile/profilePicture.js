2cb05dd0302550d63b6e0e253211eb69fc2abe2a
Nick Sardonini <nsardonini@gmail.com>
2021-03-23 15:18:43 -0400

    remove unused import

--- src/components/userProfile/profilePicture.js
+++ src/components/userProfile/profilePicture.js
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import styled from "styled-components";
-import defaultPfp from "./static/default.jpg";
 
 const StyledProfile = styled.div`
   max-width: fit-content;
