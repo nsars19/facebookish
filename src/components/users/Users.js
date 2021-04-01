78c776dc573a90c331d1d31f0ce0ead6f13f79cb
Nick Sardonini <nsardonini@gmail.com>
2021-04-01 13:04:44 -0400

    add receivedRequests & unreuqestedUsers components to users component

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,33 +1,67 @@
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import styled from "styled-components";
+import ReceivedRequests from "./receivedRequests";
+import UnrequestedUsers from "./unrequestedUsers";
 
-function Users() {
+const StyledUsers = styled.ul`
+  margin-top: 10px;
+  margin-bottom: 80px;
+  display: grid;
+  justify-items: center;
+  grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
+  gap: 4px;
+
+  li {
+    list-style: none;
+    width: fit-content;
+    height: fit-content;
+  }
+`;
+
+function Users({ currentUser }) {
   const [users, setUsers] = useState(null);
+  const [receivedReqs, setReceivedReqs] = useState(null);
+  const [userObject, setUserObject] = useState(null);
 
   useEffect(() => {
     (async function fetchUsers() {
-      const users = await fetch("http://localhost:3000/users");
-      const userData = await users.json();
+      const url = "http://localhost:3000";
+      const currentUserObj = await fetch(`${url}/users/${currentUser}`);
+      const users = await fetch(`${url}/users/find-users/${currentUser}`);
+      const received = await fetch(`${url}/friends/pending/${currentUser}`);
 
-      setUsers(userData);
+      const promiseArr = [currentUserObj.json(), users.json(), received.json()];
+
+      Promise.all(promiseArr).then((data) => {
+        const [currUser, users, recReqs] = data;
+
+        setUserObject(currUser);
+        setUsers(users);
+        setReceivedReqs(recReqs);
+      });
     })();
-  }, []);
+  }, [currentUser]);
+
+  const getCommonFriendCount = (self, user) => {
+    return self.friends.filter((friendId) => user.friends.includes(friendId))
+      .length;
+  };
 
   return (
-    (users && (
-      <div>
-        {users.map((user) => {
-          return (
-            <h2 key={user._id}>
-              <Link to={`/user/${user._id}`} className="user">
-                {user.firstName + " " + user.lastName}
-              </Link>
-            </h2>
-          );
-        })}
-      </div>
-    )) || <h2>Loading...</h2>
+    <StyledUsers>
+      <ReceivedRequests
+        currentUser={currentUser}
+        reqs={receivedReqs}
+        userData={userObject}
+        getMutualCount={getCommonFriendCount}
+      />
+      <UnrequestedUsers
+        currentUser={currentUser}
+        users={users}
+        userData={userObject}
+        getMutualCount={getCommonFriendCount}
+      />
+    </StyledUsers>
   );
 }
 
