97c04e178adf708d3790cd697f5e1cbea2782346
Nick Sardonini <nsardonini@gmail.com>
2021-04-08 17:30:52 -0400

    remove @import from createGlobalStyle & add to index.html head as per styled-components recommendations

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyle = createGlobalStyle`
   *,
   *::before,
-  *::after {
-    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap');
-       
+  *::after {       
     padding: 0;
     margin: 0;
     box-sizing: border-box;
