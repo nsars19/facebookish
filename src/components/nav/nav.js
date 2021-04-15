802d66fa87fd7e00baef34eede87273147da1f41
Nick Sardonini <nsardonini@gmail.com>
2021-04-15 18:57:48 -0400

    remove unused import

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import colors from "../../colors";
 import MainAccountSettings from "./components/mainAccountSettings";
