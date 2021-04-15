6b0dd971326215214faeec2c7bc6d63901afbd6b
Nick Sardonini <nsardonini@gmail.com>
2021-04-15 18:38:52 -0400

    add detail to PostLoader component
    adds comment skeletons

--- src/components/loader/postLoader.js
+++ src/components/loader/postLoader.js
@@ -15,7 +15,7 @@ const StyledLoader = styled.div`
   }
 
   .detail-right {
-    margin-left: 20px;
+    margin-left: 10px;
     width: 100%;
     display: grid;
     align-content: space-between;
@@ -33,19 +33,49 @@ const StyledLoader = styled.div`
   .post-body {
     height: 200px;
   }
+  .comments {
+    margin-top: 15px;
+  }
+  .comment {
+    margin-top: 12px;
+    display: grid;
+    grid-auto-flow: column;
+    justify-content: start;
+    gap: 10px;
+  }
 `;
 
 function PostLoader() {
   return (
     <StyledLoader className="loading" id="post-loader">
       <div className="post-detail">
-        <Skeleton circle="true" height={50} width={50} className="pfp" />
+        <Skeleton circle="true" height={40} width={40} className="pfp" />
         <div className="detail-right">
           <Skeleton className="author" />
           <Skeleton className="posted-at" />
         </div>
       </div>
-      <Skeleton className="text" count={4} />
+      <Skeleton className="text" count={3} />
+      <div className="comments">
+        <div className="comment">
+          <Skeleton circle={true} height={35} width={35} />
+          <div className="cmts-right">
+            <Skeleton height={55} width={200} />
+          </div>
+        </div>
+        <div className="comment">
+          <Skeleton circle={true} height={35} width={35} />
+          <div className="cmts-right">
+            <Skeleton height={55} width={260} />
+          </div>
+        </div>
+        <div className="comment">
+          <Skeleton circle={true} height={35} width={35} />
+          <div className="cmts-right">
+            <Skeleton height={55} width={150} />
+          </div>
+        </div>
+      </div>
     </StyledLoader>
   );
 }
