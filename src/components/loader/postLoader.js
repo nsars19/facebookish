370cb4b5c92baf3d92268a0b57c1ac578ee51072
Nick Sardonini <nsardonini@gmail.com>
2021-04-16 19:14:33 -0400

    add SkeletonTheme wrapper component around PostLoader
    changes color & highlight color for better visibility

--- src/components/loader/postLoader.js
+++ src/components/loader/postLoader.js
@@ -1,4 +1,4 @@
-import Skeleton from "react-loading-skeleton";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import styled from "styled-components";
 
 const StyledLoader = styled.div`
@@ -51,34 +51,36 @@ const StyledLoader = styled.div`
 function PostLoader() {
   return (
     <StyledLoader className="loading" id="post-loader">
-      <div className="post-detail">
-        <Skeleton circle="true" height={40} width={40} className="pfp" />
-        <div className="detail-right">
-          <Skeleton className="author" />
-          <Skeleton className="posted-at" />
-        </div>
-      </div>
-      <Skeleton className="text" count={3} />
-      <div className="comments">
-        <div className="comment">
-          <Skeleton circle={true} height={35} width={35} />
-          <div className="cmts-right">
-            <Skeleton height={55} width={200} />
+      <SkeletonTheme color={"#d8d8d8"} highlightColor={"#f0f0f0"}>
+        <div className="post-detail">
+          <Skeleton circle="true" height={40} width={40} className="pfp" />
+          <div className="detail-right">
+            <Skeleton className="author" />
+            <Skeleton className="posted-at" />
           </div>
         </div>
-        <div className="comment">
-          <Skeleton circle={true} height={35} width={35} />
-          <div className="cmts-right">
-            <Skeleton height={55} width={260} />
+        <Skeleton className="text" count={3} />
+        <div className="comments">
+          <div className="comment">
+            <Skeleton circle={true} height={35} width={35} />
+            <div className="cmts-right">
+              <Skeleton height={55} width={200} />
+            </div>
+          </div>
+          <div className="comment">
+            <Skeleton circle={true} height={35} width={35} />
+            <div className="cmts-right">
+              <Skeleton height={55} width={260} />
+            </div>
+          </div>
+          <div className="comment">
+            <Skeleton circle={true} height={35} width={35} />
+            <div className="cmts-right">
+              <Skeleton height={55} width={150} />
+            </div>
           </div>
         </div>
-        <div className="comment">
-          <Skeleton circle={true} height={35} width={35} />
-          <div className="cmts-right">
-            <Skeleton height={55} width={150} />
-          </div>
-        </div>
-      </div>
+      </SkeletonTheme>
     </StyledLoader>
   );
 }
