22f1b5f803cd996d607b9c60c57d78137b89fdfc
Nick Sardonini <nsardonini@gmail.com>
2021-04-21 14:39:27 -0400

    remove Courier as default font

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     margin: 0;
     box-sizing: border-box;
-    font-family: Courier, Arial, sans-serif;
+    font-family: Arial, sans-serif;
   }
     
   a.user {
