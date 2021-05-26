75eae218aa9bb0ff60d5e04ccc627457c6782589
Nick S <nsardonini@gmail.com>
2021-05-26 11:17:30 -0400

    add cookie functionality to theme context component

--- src/components/themeContext/themeContext.js
+++ src/components/themeContext/themeContext.js
@@ -1,5 +1,6 @@
 import { useState, useContext, createContext } from "react";
 import { GlobalStyle } from "./../globalTheme/globalTheme";
+import Cookies from "universal-cookie";
 
 const ThemeColorContext = createContext();
 const UpdateThemeContext = createContext();
@@ -12,10 +13,16 @@ export const useUpdateTheme = () => {
   return useContext(UpdateThemeContext);
 };
 
-export default function ThemeContext({ children }) {
-  const [lightTheme, setLightTheme] = useState(true);
+const cookies = new Cookies();
 
-  const toggleThemeColor = () => setLightTheme((theme) => !theme);
+export default function ThemeContext({ children }) {
+  const isLight = cookies.get("light") === "true" ? true : false;
+  const [lightTheme, setLightTheme] = useState(isLight);
+
+  const toggleThemeColor = () => {
+    cookies.set("light", !lightTheme);
+    setLightTheme((theme) => !theme);
+  };
 
   return (
     <ThemeColorContext.Provider value={lightTheme}>
